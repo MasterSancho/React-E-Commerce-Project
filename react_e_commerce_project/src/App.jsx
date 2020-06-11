807efd08e3a0bcad091a4ffbeba3edbde9b2f9a2
MasterSancho <57605252+MasterSancho@users.noreply.github.com>
2020-06-11 23:09:29 +0300

    008-Sign In Component

--- react_e_commerce_project/src/App.jsx
+++ react_e_commerce_project/src/App.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/Home_Page/HomePage';
 import ShopPage from './pages/Shop_Page/ShopPage';
+import SignInAndSignUp from './pages/Sign-In-And-Sign-Up/Sign-In-And-Sign-Up';
 import Header from './components/Header/Header';
 
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
             <Switch>
                 <Route exact path='/' component={HomePage} />
                 <Route path='/shop' component={ShopPage} />
+                <Route path='/signIn' component={SignInAndSignUp} />
             </Switch>
         </div>
     );
---
+++ react_e_commerce_project/src/pages/Sign-In-And-Sign-Up/Sign-In-And-Sign-Up.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignIn from '../../components/Sign_In/Sign-In';
+
+import './Sign-In-And-Sign-Up.scss';
+
+const SignInAndSignUp = () => (
+    <div className='sign-in-sign-up'>
+        <SignIn />
+    </div>
+);
+
+export default SignInAndSignUp;
