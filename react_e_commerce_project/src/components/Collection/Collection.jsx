6d80e4f6d2eb450f0bf559441ce067acdb14bd95
MasterSancho <57605252+MasterSancho@users.noreply.github.com>
2020-06-11 19:45:22 +0300

    006-Collection Item & Style

--- react_e_commerce_project/src/components/Collection/Collection.jsx
+++ react_e_commerce_project/src/components/Collection/Collection.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CollectionItem from '../Collection_Item/CollectionItem';
 
 import './Collection.scss';
 
@@ -8,8 +9,8 @@ const Collection = ({ title, items }) => (
         <div className='preview'>
             {items
                 .filter((item, idx) => idx < 4)
-                .map(item => (
-                    <div key={item.id}>{item.name}</div>
+                .map(({ id, ...otherItemProps }) => (
+                    <CollectionItem key={id} {...otherItemProps} />
                 ))}
         </div>
     </div>
