900297a184b809a8d3fc5ea64857d659f76f6da3
MasterSancho <57605252+MasterSancho@users.noreply.github.com>
2020-06-11 22:16:57 +0300

    007-Header Component (Link)

--- react_e_commerce_project/src/App.jsx
+++ react_e_commerce_project/src/App.jsx
@@ -3,13 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/Home_Page/HomePage';
 import ShopPage from './pages/Shop_Page/ShopPage';
+import Header from './components/Header/Header';
 
 import './App.css';
-
 
 function App() {
     return (
         <div>
+            <Header />
             <Switch>
                 <Route exact path='/' component={HomePage} />
                 <Route path='/shop' component={ShopPage} />
