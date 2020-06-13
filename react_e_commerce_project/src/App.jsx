00d95fbdce098e6873631b28fd9ccf1b9d21af18
MasterSancho <57605252+MasterSancho@users.noreply.github.com>
2020-06-13 10:57:19 +0300

    013-Google Sign In Auth in App.js with ComponentDidMount()

--- react_e_commerce_project/src/App.jsx
+++ react_e_commerce_project/src/App.jsx
@@ -5,20 +5,45 @@ import HomePage from './pages/Home_Page/HomePage';
 import ShopPage from './pages/Shop_Page/ShopPage';
 import SignInAndSignUp from './pages/Sign-In-And-Sign-Up/Sign-In-And-Sign-Up';
 import Header from './components/Header/Header';
+import { auth } from './Firebase/firebase.util';
 
 import './App.css';
 
-function App() {
-    return (
-        <div>
-            <Header />
-            <Switch>
-                <Route exact path='/' component={HomePage} />
-                <Route path='/shop' component={ShopPage} />
-                <Route path='/signIn' component={SignInAndSignUp} />
-            </Switch>
-        </div>
-    );
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            currentUser: null,
+        };
+    }
+
+    unsubscribeFromAuth = null;
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+            this.setState({ currentUser: user });
+
+            console.log(user);
+        });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route exact path='/' component={HomePage} />
+                    <Route path='/shop' component={ShopPage} />
+                    <Route path='/signIn' component={SignInAndSignUp} />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
