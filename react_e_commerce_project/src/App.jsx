b81b5c647de4f5c17847335c331aa4edc56f0ccc
MasterSancho <57605252+MasterSancho@users.noreply.github.com>
2020-06-17 22:39:58 +0300

    035-Checkout Page

--- react_e_commerce_project/src/App.jsx
+++ react_e_commerce_project/src/App.jsx
@@ -6,8 +6,12 @@ import { createStructuredSelector } from 'reselect';
 import HomePage from './pages/Home_Page/HomePage';
 import ShopPage from './pages/Shop_Page/ShopPage';
 import SignInAndSignUp from './pages/Sign-In-And-Sign-Up/Sign-In-And-Sign-Up';
+import CheckoutPage from './pages/Checkout_Page/CheckoutPage'
+
 import Header from './components/Header/Header';
+
 import { auth, createUserProfileDocument } from './Firebase/firebase.util';
+
 import { setCurrentUser } from './Redux/user/user.actions';
 import { selectCurrentUser } from './Redux/user/user.selectors';
 
@@ -46,6 +50,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HomePage} />
                     <Route path='/shop' component={ShopPage} />
+                    <Route exact path='/checkout' component={CheckoutPage} />
                     <Route
                         exact
                         path='/signin'
