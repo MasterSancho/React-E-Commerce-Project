cf796c42c9a8603fe1243d5e02d1fbbef8f9da2f
MasterSancho <57605252+MasterSancho@users.noreply.github.com>
2020-06-17 19:58:26 +0300

    033-Reselect Library
    npm install reselect

--- react_e_commerce_project/src/components/Cart_Dropdown/Cart-Dropdown.jsx
+++ react_e_commerce_project/src/components/Cart_Dropdown/Cart-Dropdown.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import CustomButton from '../Custom_Button/Custom-Button';
 import CartItem from '../Cart_Item/Cart-Item';
+import { selectCartItems } from '../../Redux/cart/cart.selectors';
 
 import './Cart-Dropdown.scss';
 
@@ -17,8 +18,8 @@ const CartDropdown = ({ cartItems }) => (
     </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    cartItems,
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropdown);
