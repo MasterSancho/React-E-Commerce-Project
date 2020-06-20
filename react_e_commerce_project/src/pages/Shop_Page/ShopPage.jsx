236cca52b6a0f1d7db15858f54a11baa7ad3a877
MasterSancho <57605252+MasterSancho@users.noreply.github.com>
2020-06-20 11:26:21 +0300

    042-Collection State Into Redux

--- react_e_commerce_project/src/pages/Shop_Page/ShopPage.jsx
+++ react_e_commerce_project/src/pages/Shop_Page/ShopPage.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import Collection from '../../components/Collection/Collection';
+import { selectCollections } from '../../Redux/shop/shop.selector';
 
-import SHOP_DATA from './ShopData';
+const ShopPage = ({ collections }) => (
+    <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+            <Collection key={id} {...otherCollectionProps} />
+        ))}
+    </div>
+);
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections,
+});
 
-        this.state = {
-            collections: SHOP_DATA,
-        };
-    }
-
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-                {collections.map(({ id, ...otherCollectionProps }) => (
-                    <Collection key={id} {...otherCollectionProps} />
-                ))}
-            </div>
-        );
-    }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
