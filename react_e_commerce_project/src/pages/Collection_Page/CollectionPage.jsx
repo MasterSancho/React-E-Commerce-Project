7e1f2a58fe92ce8f406217a96c47a57f846f9853
MasterSancho <57605252+MasterSancho@users.noreply.github.com>
2020-06-20 20:23:29 +0300

    044-Improving naming of component

---
+++ react_e_commerce_project/src/pages/Collection_Page/CollectionPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CollectionItem from '../../components/Collection_Item/CollectionItem';
+
+import './CollectionPage.scss';
+
+const CollectionPage = ({ match }) => (
+    <div className='collection-page'>
+        <h2>COLLECTION PAGE</h2>
+    </div>
+);
+
+export default CollectionPage;
--- react_e_commerce_project/src/pages/Shop_Page/ShopPage.jsx
+++ react_e_commerce_project/src/pages/Shop_Page/ShopPage.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/Collections_Overview/Collections-Overview';
-import CategoryPage from '../Category_Page/CategoryPage';
+import CollectionPage from '../Collection_Page/CollectionPage';
 
 const ShopPage = ({ match }) => (
     <div className='shop-page'>
         <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 );
 
