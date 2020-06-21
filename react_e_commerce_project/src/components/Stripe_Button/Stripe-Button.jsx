27878135a46630777897a8e4aba546790eabdfaf
MasterSancho <57605252+MasterSancho@users.noreply.github.com>
2020-06-21 09:22:33 +0300

    047-Stripe Integration

---
+++ react_e_commerce_project/src/components/Stripe_Button/Stripe-Button.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey =
+        'pk_test_51GwLyJCGWnk9ofKLPCNQ9CPAfFSsd3NhxuqGk63BioomvqO4nvU99GPugdDwUZu5U0dOSHen023YaUbMon2GUx1J00R4zUSt3e';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    };
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='E_Commerce'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
