0a62ecf82eafe405f530527049da4c4e9249f1ab
MasterSancho <57605252+MasterSancho@users.noreply.github.com>
2020-06-26 23:46:07 +0300

    051-Styled components in app 3

--- react_e_commerce_project/src/components/Custom_Button/Custom-Button.jsx
+++ react_e_commerce_project/src/components/Custom_Button/Custom-Button.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import './Custom-Button.scss';
+import { CustomButtonContainer } from './Custom-Button.style';
 
-const CustomButton = ({
-    children,
-    isGoogleSignIn,
-    inverted,
-    ...otherProps
-}) => (
-    <button
-        className={`${inverted ? 'inverted' : ''} ${
-            isGoogleSignIn ? 'google-sign-in' : ''
-        } custom-button `}
-        {...otherProps}
-    >
-        {children}
-    </button>
+const CustomButton = ({ children, ...props }) => (
+    <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
 );
 
 export default CustomButton;
