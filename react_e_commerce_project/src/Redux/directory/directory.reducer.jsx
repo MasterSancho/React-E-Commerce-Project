ef0a4a5447a066d944cb8138ed7243445bf1c768
MasterSancho <57605252+MasterSancho@users.noreply.github.com>
2020-06-29 13:33:26 +0300

    054-Update

--- react_e_commerce_project/src/Redux/directory/directory.reducer.jsx
+++ react_e_commerce_project/src/Redux/directory/directory.reducer.jsx
@@ -3,28 +3,28 @@ const INITIAL_STATE = {
         {
             title: 'hats',
             // imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-            imageUrl: 'images/hats.jpg', // local file in public/images/
+            imageUrl: 'images/hats.jpg', 
             id: 1,
             linkUrl: 'shop/hats',
         },
         {
             title: 'jackets',
             // imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-            imageUrl: 'images/jackets.png', // local file in public/images/
+            imageUrl: 'images/jackets.png', 
             id: 2,
             linkUrl: 'shop/jackets',
         },
         {
             title: 'sneakers',
             // imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-            imageUrl: 'images/sneakers.png', // local file in public/images/
+            imageUrl: 'images/sneakers.png', 
             id: 3,
             linkUrl: 'shop/sneakers',
         },
         {
             title: 'womens',
             // imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-            imageUrl: 'images/womens.jfif', // local file in public/images/
+            imageUrl: 'images/womens.jfif', 
             size: 'large',
             id: 4,
             linkUrl: 'shop/womens',
@@ -32,7 +32,7 @@ const INITIAL_STATE = {
         {
             title: 'mens',
             // imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-            imageUrl: 'images/men.jpg', // local file in public/images/
+            imageUrl: 'images/men.jpg', 
             size: 'large',
             id: 5,
             linkUrl: 'shop/mens',
